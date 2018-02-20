6571e9f440deeb081822163cd8be186e2cf54e38
Philippe Vanzin Moreira <pvanzinmoreira@gmail.com>
2018-02-19 21:21:32 -0300

    [Add] Category Fixtures

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    private function data()
+    {
+        return [
+            [
+                'name' => 'PHP Syntax',
+                'description' => 'PHP Syntax Description'
+            ]
+        ];
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        foreach($this->data() as $data){
+
+            $category = new Category();
+            $category
+                ->setName($data['name'])
+                ->setDescription($data['description']);
+
+            $manager->persist($category);
+        }
+
+        $manager->flush();
+    }
+}
