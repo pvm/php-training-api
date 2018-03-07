764c8b76c9a772bb9b22a40927d1ceaed075719e
Philippe Moreira <philippe.moreira@engesoftware.com.br>
2018-03-07 17:31:06 -0300

    [Mod] Reset Database for tests

--- tests/ApiTestCase.php
+++ tests/ApiTestCase.php
@@ -3,28 +3,77 @@
 namespace App\Tests;
 
 use Doctrine\Common\DataFixtures\Purger\ORMPurger;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Console\Input\StringInput;
 
 class ApiTestCase extends WebTestCase
 {
-    private $client;
+    protected static $application;
+
+    protected $client;
+
+    protected $container;
+
+    protected $entityManager;
 
     public function setUp()
     {
+        $this->tearUp();
+
+        $this->client = static::createClient([], [
+            'HTTP_ACCEPT' => 'application/json'
+        ]);
+        $this->container = $this->client->getContainer();
+        $this->entityManager = $this->container->get('doctrine.orm.entity_manager');
+
         parent::setUp();
-        $this->purgeDatabase();
     }
 
-    private function purgeDatabase()
+    public function tearUp()
     {
-        $entityManager = $this->getService('doctrine')->getManager();
-
-        $purger = new ORMPurger($entityManager);
-        $purger->purge();
+        self::runCommand('doctrine:database:drop --force');
+        self::runCommand('doctrine:database:create');
+        self::runCommand('doctrine:schema:create');
+        self::runCommand('doctrine:fixtures:load --append --no-interaction');
     }
 
-    private function getService($id)
+    /**
+     * {@inheritDoc}
+     */
+    protected function tearDown()
     {
-        return self::$kernel->getContainer()->get($id);
+        self::runCommand('doctrine:database:drop --force');
+
+        parent::tearDown();
+
+        $this->entityManager->close();
+        $this->entityManager = null; // avoid memory leaks
+    }
+
+    /**
+     * Run a command
+     *
+     * @param $command
+     * @return int
+     * @throws \Exception
+     */
+    protected static function runCommand($command)
+    {
+        $command = sprintf('%s --quiet', $command);
+        return self::getApplication()->run(new StringInput($command));
+    }
+
+
+    protected static function getApplication()
+    {
+        if (null === self::$application) {
+            $client = static::createClient();
+
+            self::$application = new Application($client->getKernel());
+            self::$application->setAutoExit(false);
+        }
+
+        return self::$application;
     }
 }
